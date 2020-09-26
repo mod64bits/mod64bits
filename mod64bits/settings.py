462ac4b579a99b42487d50a3a21d0c856c434456
mod64bits <mod64bits@gmail.com>
2020-09-26 01:37:48 -0300

    Preparação da home

--- mod64bits/settings.py
+++ mod64bits/settings.py
@@ -108,8 +108,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATE_INPUT_FORMATS = ['%d/%m/%Y']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
