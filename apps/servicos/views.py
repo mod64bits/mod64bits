04814eaac89289aed855a1773789bba172d1209d
mod64bits <mod64bits@gmail.com>
2020-09-28 02:38:51 -0300

    Configuração db servicos e categoria serviços

--- apps/servicos/views.py
+++ apps/servicos/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView
+from .models import CategoriaServicos
 
-# Create your views here.
+
+class NovoServicoView(CreateView):
+    model = CategoriaServicos
+    context_object_name = 'categoriaServicos'
+    template_name = 'servicos/nova_categoria_servico.html'
+
+
+class ListaCategoriaServico(ListView):
+    model = CategoriaServicos
+    template_name = 'servicos/lista_categoria_servico.html'
+
+
+class EditarCategoriaServico(UpdateView):
+    model = CategoriaServicos
+    fields = '__all__'
+    template_name = 'servicos/editar_categoria_servico.html'
